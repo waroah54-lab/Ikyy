1d5b3fceb66dfc1260c9e39c6b8d1d6f4f8b3cab
waroah54-lab <waroah54@gmail.com>
2025-11-27 15:04:02 +0700

    Create bluearchive.js

---
+++ src/api/random/bluearchive.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+module.exports = function (app) {
+    async function bluearchive() {
+        try {
+            const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`)
+            const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
+            return Buffer.from(response.data);
+        } catch (error) {
+            throw error;
+        }
+    }
+    app.get('/random/blue-archive', async (req, res) => {
+        const { apikey } = req.query;
+        if (!apikey) return res.status(400).json({
+            status: false,
+            error: 'apikey is required'
+        });
+
+        if (!global.apikey.includes(apikey)) return res.status(403).json({
+            status: false,
+            error: 'invalid apikey'
+        });
+
+        try {
+            const pedo = await bluearchive();
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': pedo.length,
+            });
+            res.end(pedo);
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+    });
+};
+
