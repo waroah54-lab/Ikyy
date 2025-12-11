b8348bef41eb2e9d7e8ed3b52225e1452cee5ac1
waroah54-lab <waroah54@gmail.com>
2025-12-11 11:41:16 +0700

    Update api.js

--- src/api/api/api.js
+++ src/api/api/api.js
@@ -1,29 +1,33 @@
 module.exports = function (app) {
 
 function listRoutes() {
+    if (!app._router || !app._router.stack) return 0;
+
     let anuan = app._router.stack
         .filter(layer => layer.route)
         .map(layer => ({
             method: Object.keys(layer.route.methods).join(', ').toUpperCase(),
             path: layer.route.path
-        }));        
-     return anuan.length - 1
+        }));
+
+    // jika ingin exclude path ini
+    return anuan.filter(r => r.path !== "/api/status").length;
 }
 
 app.get('/api/status', async (req, res) => {
-        try {
-            res.status(200).json({
-                status: true,
-                result: {
-                status: "Aktif", 
-                totalrequest: global.totalreq.toString(), 
-                totalfitur: `${listRoutes()}`, 
-                runtime: runtime(process.uptime()), 
+    try {
+        res.status(200).json({
+            status: true,
+            result: {
+                status: "Aktif",
+                totalrequest: global.totalreq.toString(),
+                totalfitur: listRoutes(),
+                runtime: runtime(process.uptime()),
                 domain: req.hostname
-                }
-            });
-        } catch (error) {
-            res.status(500).send(`Error: ${error.message}`);
-        }
+            }
+        });
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
 });
 }
